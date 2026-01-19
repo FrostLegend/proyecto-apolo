b98bcc3803a9e1c6f5a6d9b6add8e8d127504ef5
«FrostLegend» <alejandro11valla@gmail.com>
2026-01-19 13:30:55 +0100

    Supabase datos

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://vjunfxedwdmoibnfbzoi.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqdW5meGVkd2Rtb2libmZiem9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MzIwNjUsImV4cCI6MjA3NjEwODA2NX0.1l8AI_vIcoGZVJzQ7SL15QihGav_VQCnxqFZp854ZV8',
+};
