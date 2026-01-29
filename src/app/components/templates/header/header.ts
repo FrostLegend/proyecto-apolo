46b37297bef574767f494a61e690f732690d4967
«FrostLegend» <alejandro11valla@gmail.com>
2026-01-29 11:40:01 +0100

    login y resources

--- src/app/components/templates/header/header.ts
+++ src/app/components/templates/header/header.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Supaservice } from '../../../services/supaservice';
+import { map, Observable, Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  supaservice: Supaservice = inject(Supaservice);
+  
+  sesion: string | null = null;
+  suscriptor = new Subscription;
 
+  /*constructor(){
+    this.suscriptor = this.supaservice.authChangesObservable().subscribe(({ event, session }) =>{
+      this.sesion = session?.user?.email || null;
+      console.log(session?.user.email);
+      console.log(event);
+    });
+  }*/
+
+  sesion$ = this.supaservice.authChangesObservable().pipe(
+    map(({ session }) => session?.user?.email ?? null)
+  );
+
+  logOut(){
+    this.supaservice.logout();
+  }
+
+  ngOnDestroy(){
+    this.suscriptor?.unsubscribe();
+  }
 }
--- src/app/services/supaservice.ts
+++ src/app/services/supaservice.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { from, Observable } from 'rxjs';
+import { inject, Injectable, resource } from '@angular/core';
+import { AuthChangeEvent, createClient, Session, SupabaseClient } from '@supabase/supabase-js';
+import { from, Observable, subscribeOn } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Planta } from '../plantas/planta';
 
@@ -16,6 +16,9 @@ export class Supaservice {
 
   constructor(){
     this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+    this.authChangesObservable().subscribe(() =>{
+      this.plantasResource.reload();
+    });
   }
 
   async getPlantasSupabase(){
@@ -42,7 +45,7 @@ export class Supaservice {
     return data;
   }
 
-  getPlantas(): Observable<Planta[]>{
+  /*getPlantas(): Observable<Planta[]>{
     return this.http.get<Planta[]>(environment.supabaseUrl+"/rest/v1/plantas?select=*",{
       headers: new HttpHeaders({
         apikey: environment.supabaseKey,
@@ -58,7 +61,7 @@ export class Supaservice {
         Authorization: `Bearer ${environment.supabaseKey}`
       })
     })
-  }
+  }*/
 
   async login(loginData: {email: string, password: string}){
     let { data, error } = await this.supabase.auth.signInWithPassword(loginData);
@@ -67,5 +70,35 @@ export class Supaservice {
       throw error;
     }
     return data;
+  }
+
+  plantasResource = resource({
+    params:()=>({}),
+    loader: async()=> {
+      return await this.getPlantasSupabase();
+    }
+  })
+
+  authChanges(callback: (event: AuthChangeEvent, session: Session | null) => void){
+    return this.supabase.auth.onAuthStateChange(callback); // Recivir una notificación con cada envento de autentificación
+  }
+
+  authChangesObservable():Observable<{event: AuthChangeEvent; session: Session | null}>{
+    return new Observable((subscriber) => {
+      const {data: authListener } = this.authChanges(
+      // Funcion Callback que le pasamaos a authChanges de arriba
+      (event: AuthChangeEvent, session: Session | null) => {
+        // Con cada evento de autentificación los suscriptores reciben un aviso
+        subscriber.next({event, session});
+      },
+    );
+      return () => {
+        authListener.subscription.unsubscribe();
+      }
+    });
+  }
+
+  async logout(){
+    let {error} = await this.supabase.auth.signOut();
   }
 }
